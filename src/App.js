40286b34ff61ba80ee439a76b3b16e0d5490dc05
Megan Cindric <megancindric@gmail.com>
2023-05-11 14:47:52 -0600

    📦 NEW: Adding Tailwind, custom fonts, and pokeapi-js-wrapper

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import DetailsPage from './pages/DetailsPage/DetailsPage';
-import SearchPage from './pages/SearchPage/SearchPage';
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import DetailsPage from "./pages/DetailsPage/DetailsPage";
+import SearchPage from "./pages/SearchPage/SearchPage";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
+    <div className="App custom_font">
+      <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/search" element={<SearchPage/>}/>
-        <Route path="/details/:pokeName" element={<DetailsPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/details/:pokeName" element={<DetailsPage />} />
       </Routes>
     </div>
   );
